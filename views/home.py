97cb41ed060510ded591018ebfc6877df1c9a516
renegng <renegng@gmail.com>
2021-08-01 23:50:52 -0600

    Stable version

--- views/home.py
+++ views/home.py
@@ -488,41 +488,40 @@ class WassengerTask(threading.Thread):
                                     msg_request = json.dumps(payload)
                                 )
                                 
-                                # response = requests.request("POST", self.url, json=payload, headers=self.headers)
+                                response = requests.request("POST", self.url, json=payload, headers=self.headers)
                                 
-                                # sent_msg.msg_response = _returnResponseJSON(response)
-                                # sent_msg.date_response = dt.now(tz.utc)
-                                # db.session.add(sent_msg)
+                                sent_msg.msg_response = _returnResponseJSON(response)
+                                sent_msg.date_response = dt.now(tz.utc)
+                                db.session.add(sent_msg)
                                 
-                                # # Update Sent Message Progress Record
-                                # if self.progressid is not None:
-                                #     self.progressid.msg_last_usr = json.dumps({
-                                #             'uid': user.id,
-                                #             'uname': user.name,
-                                #             'uphone': user.phonenumber
-                                #     })
-                                #     self.progressid.msg_sent_detail = json.dumps(payload)
-                                #     self.progressid.msg_sent_amount += 1
-                                #     flag_modified(self.progressid, 'msg_last_usr')
-                                #     flag_modified(self.progressid, 'msg_sent_detail')
+                                # Update Sent Message Progress Record
+                                if self.progressid is not None:
+                                    self.progressid.msg_last_usr = json.dumps({
+                                            'uid': user.id,
+                                            'uname': user.name,
+                                            'uphone': user.phonenumber
+                                    })
+                                    self.progressid.msg_sent_detail = json.dumps(payload)
+                                    self.progressid.msg_sent_amount += 1
+                                    flag_modified(self.progressid, 'msg_last_usr')
+                                    flag_modified(self.progressid, 'msg_sent_detail')
                                 
-                                # # Create Sent Message Progress Record
-                                # else:
-                                #     self.progressid = SentMessagesProgress(
-                                #         msg_last_usr = json.dumps({
-                                #             'uid': user.id,
-                                #             'uname': user.name,
-                                #             'uphone': user.phonenumber
-                                #         }),
-                                #         msg_sent_detail = json.dumps(payload),
-                                #         msg_sent_amount = 1
-                                #     )
-                                #     db.session.add(self.progressid)
+                                # Create Sent Message Progress Record
+                                else:
+                                    self.progressid = SentMessagesProgress(
+                                        msg_last_usr = json.dumps({
+                                            'uid': user.id,
+                                            'uname': user.name,
+                                            'uphone': user.phonenumber
+                                        }),
+                                        msg_sent_detail = json.dumps(payload),
+                                        msg_sent_amount = 1
+                                    )
+                                    db.session.add(self.progressid)
                                 
-                                # db.session.commit()
-                                # db.session.refresh(self.progressid)
+                                db.session.commit()
+                                db.session.refresh(self.progressid)
                                 
-                                print(user.name)
                                 time.sleep(self.sleeptime)
                                 
                     # Validate WhatsApp Number
@@ -531,39 +530,39 @@ class WassengerTask(threading.Thread):
                             payload['phone'] = user.phonenumber
                             
                             retries = 1
-                            # response = requests.request("POST", self.url, json=payload, headers=self.headers)
-                            # json_response = _returnResponseJSON(response)
-                            # time.sleep(self.sleeptime)
+                            response = requests.request("POST", self.url, json=payload, headers=self.headers)
+                            json_response = _returnResponseJSON(response)
+                            time.sleep(self.sleeptime)
 
-                            # # Try to validate the number 2 more times
-                            # if 'exists' in json_response and json_response['exists'] is not True:
-                            #     retries = 2
-                            #     response = requests.request("POST", self.url, json=payload, headers=self.headers)
-                            #     json_response = _returnResponseJSON(response)
-                            #     time.sleep(self.sleeptime)
+                            # Try to validate the number 2 more times
+                            if 'exists' in json_response and json_response['exists'] is not True:
+                                retries = 2
+                                response = requests.request("POST", self.url, json=payload, headers=self.headers)
+                                json_response = _returnResponseJSON(response)
+                                time.sleep(self.sleeptime)
 
-                            #     if 'exists' in json_response and json_response['exists'] is not True:
-                            #         retries = 3
-                            #         response = requests.request("POST", self.url, json=payload, headers=self.headers)
-                            #         json_response = _returnResponseJSON(response)
-                            #         time.sleep(self.sleeptime)
+                                if 'exists' in json_response and json_response['exists'] is not True:
+                                    retries = 3
+                                    response = requests.request("POST", self.url, json=payload, headers=self.headers)
+                                    json_response = _returnResponseJSON(response)
+                                    time.sleep(self.sleeptime)
                             
-                            # # Update User Information
-                            # usr_upd = User.query.filter(
-                            #     User.id == user.id
-                            # ).first()
-                            # usr_upd.has_whatsapp = json_response['exists'] if 'exists' in json_response else False
-                            # usr_upd.comments = json.dumps({ 
-                            #     'val_wha': { 
-                            #         'attempts': retries,
-                            #         'status': response.status_code if response is not None else 410,
-                            #         'timestamp': str(dt.now(tz.utc))
-                            #     }
-                            # })
+                            # Update User Information
+                            usr_upd = User.query.filter(
+                                User.id == user.id
+                            ).first()
+                            usr_upd.has_whatsapp = json_response['exists'] if 'exists' in json_response else False
+                            usr_upd.comments = json.dumps({ 
+                                'val_wha': { 
+                                    'attempts': retries,
+                                    'status': response.status_code if response is not None else 410,
+                                    'timestamp': str(dt.now(tz.utc))
+                                }
+                            })
 
-                            # # Commit current data page updates
-                            # db.session.add(usr_upd)
-                            # db.session.commit()
+                            # Commit current data page updates
+                            db.session.add(usr_upd)
+                            db.session.commit()
 
                     
                     if self.userlist.has_next:
